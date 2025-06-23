06d2c3459ed1a0a90c84236d1776f267be2f28ca
Lera <valeria.usatenko@gmail.com>
2025-06-23 14:39:41 +0200

    Adding user sighn up form

---
+++ inscription.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Sign up page</h1>
+     <div>
+            <form action="utilisateur.php" method="get">
+                <fieldset>
+            <legend>Données de connexion</legend>
+        <div>
+            <!-- in label use the id of input "username", just good practice: if you push on label the user goes inside input -->
+             <!--autocomplete nopt to show previously entered values  -->
+            <label for="username">Nom d'utilisateur</label> 
+            <input type="text" id="username" autocomplete="off">
+        </div>
+        <div>
+            <!-- for passport use type PASSWORD -->
+            <label for="password">Your password</label> 
+            <input type="password" id="password">
+        </div>
+         </fieldset>
+         <div>
+            <!--  type NUMBER -->
+            <label for="age">Your age</label> 
+            <input type="number" id="age" min="0" max="99">
+        </div>
+           <div>
+            <!-- Choosing pre-defined value from a list -->
+             <!-- selected will be shown or by hidden or disabled option -->
+            <label for="ville">Ville</label>
+            <select id="ville">
+                <!-- <option selected disabled>Merci de choisir votre ville de résidence</option> -->
+                <option value="Marseille">Marseille</option>
+                <option value="Lyon" selected>Lyon</option>
+                <option value="Paris">Paris</option>
+            </select>
+        </div>
+         <div>
+            <!-- Text input -->
+            <label for="remarque">Your comments</label> 
+            <textarea id="remarque" cols="50" rows="5"></textarea>
+        </div>
+
+        <div>
+            <!-- RADIO , name insures that ONLY 1 can be choosen , otherwise use CHECKBOX -->
+            Genre
+            <input type="radio" name=genre>Homme 
+            <input type="radio" name=genre>Femme 
+        </div>
+
+        <div>
+            <!--CHECKBOX allows MANY choices at a time, -->
+            Preferences in Sport
+            <input type="checkbox" name=sport checked>Football 
+            <input type="checkbox" name=sport>Handball 
+            <input type="checkbox" name=sport>Tennis 
+        </div>
+        <div>
+            <!-- By default all buttons have tipe SUBMIT -->
+            <button>
+            Sign up
+        </button></div>
+    </form>
+    </div>
+</body>
+</html>
