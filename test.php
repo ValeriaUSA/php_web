ad3d1b8ac286317c008e99aab4c2602c6de4a859
Lera <valeria.usatenko@gmail.com>
2025-06-24 11:12:40 +0200

    Session manipulation

--- test.php
+++ test.php
@@ -1,3 +1,15 @@
+<?php
+// démarrer la session we declair it here because we are using $_SESSION below. This line is always starts the code
+session_start();
+$nom_c = "";
+$nom_s = "";
+if (isset($_COOKIE['nom'])) {
+    $nom_c = $_COOKIE['nom'];
+}
+if (isset($_SESSION['nom'])) {
+    $nom_s = $_SESSION['nom'];
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -8,9 +20,18 @@
 </head>
 
 <body>
-    <h1>Cookies</h1>
-    <p>Nom : <?php echo $_COOKIE['nom'] ?></p>
-    <p>Nom : <?= $_COOKIE['nom'] ?></p>
+    <h1>Cookies et sessions</h1>
+    <h2>Cookies</h2>
+    <p>Nom : <?php echo $nom_c ?></p>
+    <p>Nom : <?= $nom_c ?></p>
+    <h2>Sessions</h2>
+    <p>Nom : <?php echo $nom_s ?></p>
+    <p>Nom : <?= $nom_s ?></p>
+      <p>
+        <a href="vider_session.php">
+            Vider la session
+        </a>
+    </p>
 </body>
 
 </html>
--- utilisateur.php
+++ utilisateur.php
@@ -1,4 +1,7 @@
 <?php
+// démarrer la session
+session_start();
+
 
 $username = "Name by default"; 
 // here if the user inputs nothing we use the value above
@@ -17,7 +20,11 @@ if (isset($_REQUEST['genre']) and $_REQUEST['genre'] == 'homme') {
 $age = $_REQUEST['age'];
 $ville = $_REQUEST['ville'];
 
+// ajouter un cookie
 setcookie("nom", $username);
+
+// ajouter des données dans la session
+$_SESSION['nom'] = $username;
 ?>
 
 
@@ -37,7 +44,9 @@ setcookie("nom", $username);
         Bonjour
          <?php
         echo "$civilite $username, vous avez $age ans et êtes de $ville";
-        <a href="test.php">Consulter les cookies</a>
+
+        ?>
+        <a href="test.php">Consulter les cookies et les sessions</a>
         ?>
     </p>
 
